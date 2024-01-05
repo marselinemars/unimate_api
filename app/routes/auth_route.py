0a74346f30afe191bf9dac6273e4da3ba01ee60e
hamza djamila amani <djamila.hamza@ensia.edu.dz>
2024-01-05 00:32:31 +0000

    test

--- app/routes/auth_route.py
+++ app/routes/auth_route.py
@@ -90,11 +90,27 @@ def signup():
 @auth_bp.route('/signin', methods=['POST'])
 def signin():
     print('a login request ')
+    supabase = connect_to_supabase()
     data = request.json
 
-    return jsonify({'message': 'Sign-in failed! User information not found'}), 400
-    
-  
+    res = None
+    try:
+        res = supabase.auth.sign_in_with_password({'email': data.get('email'), 'password': data.get('password')})
+    except Exception as ex:
+        return jsonify({'message': 'Login failed','errors': [str(ex)]}), 400
+
+    user_info = res.user
+
+    if user_info:
+        user_id = user_info.id # Access the 'id' attribute from 'user_info'
+        user_email = user_info.email  # Access the 'email' attribute from 'user_info'
+        response = supabase.table('users').select('name', 'avatar_url').eq('id', user_id).execute()
+        
+        return jsonify({
+            'id': user_id, 'email': user_email, 'name': response.data[0]['name'], 'avatarUrl': response.data[0]['avatar_url']
+        }), 200
+    else:
+        return jsonify({'message': 'Sign-in failed! User information not found'}), 400
         
 @auth_bp.route('/getUserById', methods=['POST'])
 def getUserById():
