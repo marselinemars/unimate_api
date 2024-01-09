1a6958c99b20e39cc0337b1afd4f6d0ecbc5b414
hamza djamila amani <djamila.hamza@ensia.edu.dz>
2024-01-09 23:28:43 +0000

    test

--- app/routes/resources_route.py
+++ app/routes/resources_route.py
@@ -17,18 +17,13 @@ def save_resource():
 
 @resources_bp.route('/test', methods=['POST'])
 def test():
-
+    print('a login request ')
     supabase = connect_to_supabase()
+    data = request.json
 
-    data = request.form
-    
     title = data.get('title')
-    description= data.get('description')
-    type = data.get('type')
-    user_id = data.get('user_id')
-    
-    resource_data = {'title': title, 'description': description, 'type': type, 'user_id': user_id}
-    supabase.table('resources').insert(resource_data).execute()
 
     
+    return jsonify({
+            'the sent title is ': title }), 200
     return jsonify( {'title': title, 'description': description, 'type': type, 'user_id': user_id}), 200
